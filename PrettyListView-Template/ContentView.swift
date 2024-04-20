f69101a3163861530e5eefe21723b905ab2b1a56
werbowym-sheridan <werbowym@sheridancollege.ca>
2024-04-19 22:42:27 -0400

    Initial Commit

---
+++ PrettyListView-Template/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PrettyListView-Template
+//
+//  Created by Michael Werbowy on 2024-04-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PrettyListView-Template/PrettyListView_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrettyListView_TemplateApp.swift
+//  PrettyListView-Template
+//
+//  Created by Michael Werbowy on 2024-04-19.
+//
+
+import SwiftUI
+
+@main
+struct PrettyListView_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
